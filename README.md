5299000a6748b95cb9acb8929edf10fd1e1ee0f1
Timofey Trofimov <tumoxep@gmail.com>
2024-09-16 22:12:43 +0300

    revisit

--- README.md
+++ README.md
@@ -1,8 +1,8 @@
-[fastbook](https://github.com/fastai/fastbook) chapter 2 based racial features detection model working over fastapi example
+!["showcase"](readme.webp)
 
 ## why
 
-Result project that I've got while studying Chapter 2 of [fastbook](https://github.com/fastai/fastbook). Decided to create this type of model because Azure services (along with online jupyter notebook serving ones) are disabled in my country and found a repo with [thispersondoesnotexist](https://thispersondoesnotexist.com/) outputs (thanks to https://github.com/BillPlayzToday/HumanFaces)
+Result project that I've got while studying Chapter 2 of [fastbook](https://github.com/fastai/fastbook)
 
 ## info
 
@@ -10,10 +10,10 @@ Result project that I've got while studying Chapter 2 of [fastbook](https://gith
 
 ### Dataset
 
+https://github.com/BillPlayzToday/HumanFaces
+
 | feature | Images |
 | ------- | ------ |
 | white   | 137    |
 | black   | 99     |
 | asian   | 113    |
-
-![confusion matrix](confusion-matrix.png)
--- main.py
+++ main.py
@@ -1,25 +1,12 @@
 from fastai import learner
 from fastai.vision.core import PILImage
-from fastapi import FastAPI, Request, UploadFile
-from fastapi.staticfiles import StaticFiles
-from fastapi.templating import Jinja2Templates
-import uvicorn
+import gradio as gr
 
-app = FastAPI()
-app.mount("/static", StaticFiles(directory="static"), name="static")
-templates = Jinja2Templates(directory="templates")
 learn_inf = learner.load_learner('export.pkl')
 
-@app.post("/uploadfile/")
-async def create_upload_file(file: UploadFile):
-    img = PILImage.create(file.file)
-    pred,pred_idx,probs = learn_inf.predict(img)
-    return {"data": f'Prediction: {pred}; Probability: {probs[pred_idx]:.04f}'}
+def image_classifier(inp):
+    pred, pred_idx, probs = learn_inf.predict(inp)
+    return pred, float(probs[pred_idx])
 
-@app.get("/")
-def root(request: Request):
-    return templates.TemplateResponse("index.html", {"request": request})
-
-
-if __name__ == '__main__':
-    uvicorn.run(app, host='0.0.0.0', port=8000)
+demo = gr.Interface(fn=image_classifier, allow_flagging="never", inputs="image", outputs=[gr.Label(label="Prediction"), gr.Label(label="Probability")])
+demo.launch()
